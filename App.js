3f4688126db19d88d8aa34f55b030736bcbeee91
d00fy <doofy2020@icloud.com>
2019-11-21 13:44:20 +0900

    Style MemoDetailScreen

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import MemoListScreen from './src/screens/MemoListScreen';
 import Appbar from './src/components/Appbar';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoListScreen />
+      <MemoDetailScreen />
     </View >
   );
 }
--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,9 +3,21 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
     render() {
+        //スタイルをpropsでもってきて、それで上書きカスタマイズするということ。
+        const { style, color } = this.props;
+
+        let bgColor = '#E31676';
+        let textColor = '#fff';
+
+        if (color === 'white') {
+            bgColor = '#fff';
+            textColor = '#E31676';
+        }
+
         return (
-            <View style={styles.circleButton}>
-                <Text style={styles.circleButtonTitle}>
+            //配列形式で入れる、第一引数優先、それ以降は差分を見てる？？
+            <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+                <Text style={[styles.circleButtonTitle, { color: textColor }]}>
                     {this.props.children}
                 </Text>
             </View>
@@ -21,19 +33,18 @@ const styles = StyleSheet.create({
         right: 32,
         width: 48,
         height: 48,
-        backgroundColor: '#E31676',
         borderRadius: 24,
         justifyContent: 'center',
         alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
     },
     circleButtonTitle: {
         fontSize: 32,
         lineHeight: 32,
         color: '#fff',
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
     },
 });
 
