261c80d5857ca3d902574e3494000b31cb30ffac
d00fy <doofy2020@icloud.com>
2019-11-24 12:59:51 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
+import ENV from './env.json';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -9,16 +11,31 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoLoginScreen from './src/screens/LoginScreen';
 import MemoSignupScreen from './src/screens/SignupScreen';
 
+const firebaseConfig = {
+  apiKey: "ENV.FIREBASE_API_KEY",
+  authDomain: "ENV.FIREBASE_AUTH_DOMAIN",
+  databaseURL: "ENV.FIREBASE_DB_URL",
+  projectId: "ENV.FIREBASE_PRJ_URL",
+  storageBucket: "ENV.FIREBASE_STORAGE",
+  messagingSenderId: "ENV.FIREBASE_SENDER_ID",
+  appId: "ENV.FIREBASE_APP_ID",
+  measurementId: "ENV.FIREBASE_MM_ID",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const AppNavigator = createStackNavigator({
+  Login: { screen: MemoLoginScreen },
+  Signup: { screen: MemoSignupScreen },
   Home: { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit: { screen: MemoEditScreen },
-  Login: { screen: MemoLoginScreen },
-  Signup: { screen: MemoSignupScreen },
 
 }, {
   defaultNavigationOptions: {
     headerTitle: 'MemoApp',
+    headerTintColor: '#fff',
+    headerBackTitle: null,
     headerStyle: {
       backgroundColor: '#265366',
     },
